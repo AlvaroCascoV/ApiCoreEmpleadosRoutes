6bf5dc8f4391b4252be3fc20bed42bfaa5914959
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-04-27 13:13:00 +0200

    up

--- ApiCoreEmpleadosRoutes/Program.cs
+++ ApiCoreEmpleadosRoutes/Program.cs
@@ -6,7 +6,7 @@ using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-string connectionString = builder.Configuration.GetConnectionString("AZURETAJAMAR");
+string connectionString = builder.Configuration.GetConnectionString("SqlAzure");
 builder.Services.AddTransient<RepositoryEmpleados>();
 builder.Services.AddDbContext<EmpleadosContext>(options => options.UseSqlServer(connectionString));
 
